666f4c82f5dac725ed0b43dc9afc8e95d10bbb7a
Maghesree Chakraborty <mchakra2@ur.rochester.edu>
2016-10-20 12:13:20 -0400

    Added 2 tests to check tuple inputs

--- mcmc/mcmc.py
+++ mcmc/mcmc.py
@@ -1,1 +1,14 @@
-# -*- coding: utf-8 -*-
+import math
+import unittest
+class MarkovChain:
+    #def arguments(self,in_file):
+    def main(self):
+        self.dist((1,1),1)
+    def dist(self,a,b):
+        if type(a)==tuple and type(b)==tuple:
+            wt=math.sqrt((a[1]-b[1])**2+(a[0]-b[0])**2)
+            return(wt)
+
+        else:
+            print ("Inputs should be tuples")
+            raise TypeError
--- tests/test_mcmc.py
+++ tests/test_mcmc.py
@@ -8,33 +8,39 @@ test_mcmc
 Tests for `mcmc` module.
 """
 
-
+import math
 import sys
 import unittest
 from contextlib import contextmanager
 from click.testing import CliRunner
-
 from mcmc import mcmc
-from mcmc import cli
-
 
 
 class TestMcmc(unittest.TestCase):
 
     def setUp(self):
-        pass
+        #pass
+        self.m=mcmc.MarkovChain()
 
     def tearDown(self):
         pass
 
-    def test_000_something(self):
-        pass
+    def test_dist_arg_type(self):
+       
+        with self.assertRaises(TypeError):
+            self.m.dist((-4,1),1)
+        with self.assertRaises(TypeError):
+            self.m.dist(2.2,'a')
+        with self.assertRaises(TypeError):
+            self.m.dist(('a','A'),(2.2,1))
+        #self.assertRaises(TypeError,self.m.dist,[2,3])
+        #self.assertRaises(TypeError,self.m.dist,[2.3,3.2])
 
-    def test_command_line_interface(self):
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'mcmc.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+        #self.assertRaises(TypeError,self.m.dist,['a','b'])
+        #self.assertRaises(TypeError,self.m.dist,[(2,3),(3.2)])
+    def test_dist_return(self):
+        
+        val=round(math.sqrt(2.2**2+2.2**2),2)
+        wt=self.m.dist((2.2,2.2), (0,0))
+        self.assertEqual(round(wt,2),val)
+
